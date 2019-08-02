1633b4d1db3b54c27428257af1fad490201b1b1a
Stu Thompson <s.thompson@cvent.com>
2019-08-01 20:03:09 -0700

    First test fixture.

--- tests/build/cppBuildDefinition_tests.h
+++ tests/build/cppBuildDefinition_tests.h
@@ -1,7 +1,13 @@
-#include <cxxtest/TestSuite.h>
-
+#include <fstream>
+#include <iostream>
 #include <string>
-#include "../inc/build/cppBuildDefinition.h"
+
+#include <cxxtest/TestSuite.h>
+#include <nlohmann/json.hpp>
+
+#include "../../inc/build/cppBuildDefinition.h"
+
+using json = nlohmann::json;
 
 class CppBuildDefinitionTests : public CxxTest::TestSuite
 {
@@ -9,12 +15,24 @@ public:
     /**
      * Tests using the default constructor to parse a build definition.
      */
-    void ctor_test(void)
+    void testDefaultCtor(void)
     {
-        std::string jsonDef = "{\"compiler\": \"g++\"}";
-        CppBuildDefinition buildDef = CppBuildDefinition(jsonDef);
+        // Read test build definition
+        std::string testFileName = "testDef.json";
+        
+        // Create build definition object from JSON
+        CppBuildDefinition buildDef = CppBuildDefinition(testFileName);
 
         TS_TRACE("Parsing simple definition.");
         TS_ASSERT_EQUALS(buildDef.getCompiler(), "g++");
+        TS_ASSERT_EQUALS(buildDef.getCflags(), "-std=c++17 -D_REENTRANT");
+        TS_ASSERT_EQUALS(buildDef.getIncludes().size(), 2);
+        TS_ASSERT_EQUALS(buildDef.getSources().size(), 2);
+        // TS_TRACE("Testing includes.");
+        // TS_ASSERT_EQUALS(buildDef.getIncludes()[0], "file1.h");
+        // TS_ASSERT_EQUALS(buildDef.getIncludes()[1], "file2.h");
+        // TS_TRACE("Testing sources.");
+        // TS_ASSERT_EQUALS(buildDef.getSources()[0], "file1.cpp");
+        // TS_ASSERT_EQUALS(buildDef.getSources()[1], "file2.cpp");
     }
 };
